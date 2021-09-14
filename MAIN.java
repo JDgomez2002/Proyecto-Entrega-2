0094414f41e219edd98977f98eed71034474de7b
JDgomez2002 <gom21429@uvg.edu.gt>
2021-09-14 17:36:26 -0600

    Encabezados de clases
    Prueba 3

--- MAIN.java
+++ MAIN.java
@@ -4,6 +4,11 @@
 //Segundo Semestre 2021
 //Sección 11
 //Actividad: Proyecto Entrega 2
+//Grupo 8
+//José Daniel Gómez Cabrera 21429
+//Linda
+//Mario
+//Estuadro
 
 public class MAIN {
     
--- Usuarios.java
+++ Usuarios.java
@@ -1,3 +1,15 @@
+//Universidad del Valle de Guatemala
+//Programación Orientada a Objetos
+//Catedrático Tomás Gálvez
+//Segundo Semestre 2021
+//Sección 11
+//Actividad: Proyecto Entrega 2
+//Grupo 8
+//José Daniel Gómez Cabrera 21429
+//Linda
+//Mario
+//Estuadro
+
 public class Usuarios {
     
 }
