5349ab648fb3aa87f2bc869c4a84fb980aa06c71
JDgomez2002 <gom21429@uvg.edu.gt>
2021-09-14 17:25:02 -0600

    Prueba 1
    gg

--- MAIN.java
+++ MAIN.java
@@ -1,3 +1,10 @@
+//Universidad del Valle de Guatemala
+//Programación Orientada a Objetos
+//Catedrático Tomás Gálvez
+//Segundo Semestre 2021
+//Sección 11
+//Actividad: Proyecto Entrega 2
+
 public class MAIN {
     
 }
