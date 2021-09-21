7a18da7d0702193f2a0d58b57b64825b90101759
LINDA JIMÉNEZ <lijv1333@gmail.com>
2021-09-20 18:01:36 -0600

    Numeros Complejos

--- Leccion.java
+++ Leccion.java
@@ -10,6 +10,8 @@
 //Mario Antonio Guerra Morales 21008
 //Estuardo José Francisco Ayala Argueta 21315
 
+import java.util.ArrayList;
+
 public class Leccion {
     private String titulo;
     private String texto;
@@ -19,7 +21,11 @@ public class Leccion {
     private int frecuencia_visita;
 
     public void Leccion(){
-
+        
+        ArrayList<String> numeros_c = new ArrayList<String>();
+        numeros_c.add("Números Complejos;La noción de número complejo aparece ante la imposibilidad de los números reales de abarcar a las raíces de orden par del conjunto de los números negativos. Los números complejos pueden, por lo tanto, reflejar a todas las raíces de los polinomios, algo que los números reales no están en condiciones de hacer. Gracias a esta particularidad, los números complejos se emplean en diversos campos de las matemáticas, en la física y en la ingeniería. Por su capacidad para representar la corriente eléctrica y las ondas electromagnéticas, por citar un caso, son utilizados con frecuencia en la electrónica y las telecomunicaciones. Y es que el llamado análisis complejo, o sea la teoría de las funciones de este tipo, se considera una de las facetas más ricas de las matemáticas. Cabe resaltar que el cuerpo de cada número real está formado por pares ordenados (a, b). El primer componente (a) es la parte real, mientras que el segundo componente (b) es la parte imaginaria. Los números imaginarios puros son aquellos que sólo están formados por la parte imaginaria (por lo tanto, a=0). Los números complejos componen el denominado cuerpo complejo (C). Cuando el componente real a es identificado con el correspondiente complejo (a, 0), el cuerpo de estos números reales (R) se transforma en un subcuerpo de C. Por otra parte, C conforma un espacio vectorial de dos dimensiones sobre R. Esto demuestra que los números complejos no admiten la posibilidad de mantener un orden, a diferencia de los números reales.;Julián Pérez Porto");
+        System.out.println(numeros_c);
+        return;
     }
 
     public void setFrecuenciavisitas(){
