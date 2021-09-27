df1230bc5aea793d57d2bef21bfc20551cdab638
LINDA JIMÉNEZ <lijv1333@gmail.com>
2021-09-27 12:25:02 -0600

    Menu opciones

--- Leccion.java
+++ Leccion.java
@@ -13,6 +13,7 @@
 
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Clase de Lecciones 
@@ -26,6 +27,35 @@ public class Leccion {
     private String texto;
     private String cita;
     private int frecuencia_visita;
+
+    Scanner sn = new Scanner(System.in);
+
+    public int menuOpciones(){
+        boolean siguiente = true;
+        int opcion = 0;
+        while(siguiente){
+            try {
+                opcion = sn.nextInt();
+                if((opcion>0) && (opcion<7)){
+                    System.out.println();
+                    siguiente = false;
+                } else{
+                    System.out.println();
+                    System.out.println("\t\tError: ingrese un número del 1 al 6");
+                    System.out.println();
+                    sn.next();
+                }
+            } catch (Exception e) {
+                System.out.println();
+                System.out.println("\t\tError: ingrese un número válido");
+                System.out.println();
+            }
+        }
+        return opcion;
+    }
+
+
+
     /**
      * Constructor de Leccion con propiedades
      * 
